95ab60951e9fa15cbf7b4d012ef2702375d65a05
Peng Thang <sawng2020@gmail.com>
2024-04-18 00:10:25 -0500

    added one more option

--- bug-fix.py
+++ bug-fix.py
@@ -5,6 +5,7 @@ option1 = sg.Radio("Amphibians", group_id="question1")
 option2 = sg.Radio("Fish", group_id="question1")
 option3 = sg.Radio("Mammals", group_id="question1")
 option4 = sg.Radio("Birds", group_id="question1")
+option5 = sg.Radio("Dinosoar", group_id="question1")
  
 window = sg.Window("File Compressor",
                    layout=[[label,option1, option2, option3, option4]])
