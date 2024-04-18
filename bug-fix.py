6a70d2bfb16796a8e07f47767a5ec8a435f49432
Peng Thang <sawng2020@gmail.com>
2024-04-17 23:43:04 -0500

    commit

--- bug-fix.py
+++ bug-fix.py
@@ -7,11 +7,7 @@ option3 = sg.Radio("Mammals", group_id="question1")
 option4 = sg.Radio("Birds", group_id="question1")
  
 window = sg.Window("File Compressor",
-                   layout=[[label],
-                           [option1], 
-                           [option2], 
-                           [option3], 
-                           [option4]])
+                   layout=[[label,option1, option2, option3, option4]])
  
 window.read()
 window.close()
