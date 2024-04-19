aba7e5c98b2713e4b42f40acff7b26c76c807288
Peng Thang <sawng2020@gmail.com>
2024-04-19 12:38:50 -0500

    add Edit button

--- gui.py
+++ gui.py
@@ -4,9 +4,12 @@ import PySimpleGUI as sg
 lable = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+List_box =sg.Listbox(values=functions.get_todos(), key='todos', 
+                     enable_events=True, size=[45,10])
+edit_button = sg.Button("Edit")
 
 window = sg.Window('My To-Do App', 
-                   layout=[[lable],[input_box,add_button]], 
+                   layout=[[lable],[input_box,add_button], [List_box,edit_button]], 
                    font=('Helvetica',20))
 
 while True:   
@@ -19,6 +22,8 @@ while True:
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+        case "Edit":
+            todo_to_edit = values['todos']
         case sg.WIN_CLOSED:
             break
 
