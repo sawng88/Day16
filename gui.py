6bf34c9f4f5c76652bd6e7f5021f1a0b9ea5d526
Peng Thang <sawng2020@gmail.com>
2024-04-21 23:25:28 -0500

    Added edit button

--- gui.py
+++ gui.py
@@ -22,8 +22,18 @@ while True:
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
         case "Edit":
-            todo_to_edit = values['todos']
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
